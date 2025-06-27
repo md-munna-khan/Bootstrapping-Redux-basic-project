b9746e6a69176714514eac49d3b97dcf91fcfda5
md.munna <mk8761174@gmail.com>
2025-06-27 12:23:20 +0600

    22-5 Initializing Shadcn UI

--- redux-basic-project/src/App.tsx
+++ redux-basic-project/src/App.tsx
@@ -1,32 +1,10 @@
+import { Button } from "./components/ui/button"
 
-
-
-import './App.css'
-import { decrement, increment } from './redux/features/counter/counterSlice';
-
-import { useAppDispatch, useAppSelector } from './redux/hook';
-
-
-
-
-function App() {
- const dispatch = useAppDispatch();
-  const {count}=useAppSelector((state)=>state.counter)
- const handleIncrement =(amount:number)=>{
-  dispatch(increment(amount));
- }
- const handleDecrement =()=>{
-  dispatch(decrement());
- };
-
+const App = () => {
   return (
-   <div>
-    <div>Counter With Redux</div>
-    <button onClick={()=>handleIncrement(5)}>Increment by 5</button>
-    <button onClick={()=>handleIncrement(1)}>Increment</button>
-    <div>{count}</div>
-    <button onClick={handleDecrement}>Decrement</button>
-   </div>
+    <div>
+      <Button>test</Button>
+    </div>
   )
 }
 
